49736ba018faaf107e9a23589b770b5f8f3eef00
xy142 <xy142@duke.edu>
2021-09-02 14:08:15 -0400

    016

--- 016_code2/realcode2.c
+++ 016_code2/realcode2.c
@@ -7,13 +7,13 @@ int printTriangle(int size) {
     //count from 0 (inclusive) to i (inclusive), for each number j that you count
     for (int j = 0; j <= i; j++) {
       //print a "*"
-      putchar('*');
+      printf("*");
       //increment starCount
-      ++starCount;
+      starCount++;
       //when you finish counting on j,
     }
     //print a newline ("\n")
-    putchar('\n');
+    printf("\n");
     //when you finish counting on i,
   }
   //your answer is starCount
@@ -25,11 +25,10 @@ int main(void) {
   printf("Here is a triangle with height 4\n");
   numStars = printTriangle(4);
   printf("That triangle had %d total stars\n", numStars);
-  //now print "Here is a triangle with height 7\n"
-  printf("Here is a triangle with height 7\n");
+  //now print "Here is a triangle with height \n"
+  printf("Here is a triangle with heighte 7\n");
   //then call printTriangle, passing in 7, and assign the result to numStars
   numStars = printTriangle(7);
-
   //finally, print "That triangle had %d total stars\n", such that the %d
   //prints the value of numStars
   printf("That triangle had %d total stars\n", numStars);
