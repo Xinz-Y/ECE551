05fd3bcc9a6b1990d05cca4c09e50fed11b47178
xy142 <xy142@duke.edu>
2021-09-10 02:03:09 -0400

    mend

--- 028_tests_power/test-power.c
+++ 028_tests_power/test-power.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "power.h"
+
 unsigned power(unsigned x, unsigned y);
 
 void run_check(unsigned x, unsigned y, unsigned expected_ans) {
   unsigned answer = power(x, y);
   if (answer != expected_ans) {
+    printf("%s", "There is something wrong with power function\n");
     exit(EXIT_FAILURE);
   }
+  printf("%s", "There is nothing wrong with power function\n");
 }
 
 int main(void) {
@@ -20,5 +24,7 @@ int main(void) {
   run_check(5, 4, 625);
   run_check(100000, 1, 100000);
   run_check(1, 100000, 1);
+
+  printf("%s", "test are run\n");
   return EXIT_SUCCESS;
 }
