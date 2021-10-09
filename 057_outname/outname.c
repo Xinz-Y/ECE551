cbe542415cdbe2cf5c3e60c8b623c2e04c74575e
xy142 <xy142@duke.edu>
2021-10-09 00:41:11 -0400

    058

--- 057_outname/outname.c
+++ 057_outname/outname.c
@@ -1,8 +1,19 @@
+#include "outname.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "outname.h"
 
 char * computeOutputFileName(const char * inputName) {
   //WRITE ME
+  size_t n = strlen(inputName) + 1 + 7;
+  char * out_name = malloc(n * sizeof(out_name));
+  /* for(size_t i =0; i <strlen(inputName);i++ ){ */
+  /*   out_name[i] = inputName[i]; */
+  /* } */
+  strcpy(out_name, inputName);
+  char * end = ".counts";
+  strcat(out_name, end);
+
+  return out_name;
 }
