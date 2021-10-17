76d337d0cff44d38aee271b5d7821a04d8cfbcc7
xy142 <xy142@duke.edu>
2021-10-17 18:34:10 -0400

    070

--- 070_circle/circle.cpp
+++ 070_circle/circle.cpp
@@ -14,15 +14,18 @@ double Circle::intersectionArea(const Circle & otherCircle) {
   if (d < radius + otherCircle.radius) {
     double as = radius * radius;
     double bs = otherCircle.radius * otherCircle.radius;
-    double x = (as - bs + d * d) / (2 * d);
-    double zs = x * x;
-    double y = std::sqrt(as - zs);
+    double d1 = (as - bs + d * d) / (2 * d);
+    double d2 = (bs - as + d * d) / (2 * d);
+    // double zs = x * x;
+    // double y = std::sqrt(as - zs);
     // a includes in b or otherwise
     if (d <= std::abs(radius - otherCircle.radius)) {
       return M_PI * std::min(as, bs);
     }
-    return as * (std::asin(y / radius)) + bs * std::asin(y / otherCircle.radius) -
-           y * (x + std::sqrt(zs + bs - as));
+    // return as * (std::asin(y / radius)) + bs * (std::asin(y / otherCircle.radius)) -
+    //     y * ((x + std::sqrt(zs + bs - as)));
+    return as * std::acos(d1 / radius) - d1 * std::sqrt(as - d1 * d1) +
+           bs * std::acos(d2 / otherCircle.radius) - d2 * std::sqrt(bs - d2 * d2);
   }
   return 0;
 }
