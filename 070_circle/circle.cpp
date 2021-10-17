86d6c22ca871803c78eb424658f13d7dc0b8a366
xy142 <xy142@duke.edu>
2021-10-17 18:05:14 -0400

    070

--- 070_circle/circle.cpp
+++ 070_circle/circle.cpp
@@ -1,7 +1,28 @@
 #include "circle.hpp"
 
+#include <algorithm>
 #include <cmath>
+//#include "point.hpp"
+
+void Circle::move(double dx, double dy) {
+  center.move(dx, dy);
+}
 
 double Circle::intersectionArea(const Circle & otherCircle) {
-  return 11;
+  double d = center.distanceFrom(otherCircle.center);
+  // intersect
+  if (d < radius + otherCircle.radius) {
+    double as = radius * radius;
+    double bs = otherCircle.radius * otherCircle.radius;
+    double x = (as - bs + d * d) / (2 * d);
+    double zs = x * x;
+    double y = std::sqrt(as - zs);
+    // a includes in b or otherwise
+    if (d <= std::abs(radius - otherCircle.radius)) {
+      return M_PI * std::min(as, bs);
+    }
+    return as * (std::asin(y / radius)) + bs * std::asin(y / otherCircle.radius) -
+           y * (x + std::sqrt(zs + bs - as));
+  }
+  return 0;
 }
