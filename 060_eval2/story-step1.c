421cf53efbea246d42d30d9c1d6143929ea4a10a
xy142 <xy142@duke.edu>
2021-10-18 17:47:19 -0400

    test step1

--- 060_eval2/story-step1.c
+++ 060_eval2/story-step1.c
@@ -3,14 +3,13 @@
 
 #include "rand_story.h"
 
-/* int main(int argc, char ** argv) { */
-/*   if (argc != 2) { */
-/*     fprintf(stderr, "Wrong number of argc\n"); */
-/*     return EXIT_FAILURE; */
-/*   } */
-int main() {
-  // FILE * f = openfile(argv[1]);
-  FILE * f = openfile("story.txt");
+int main(int argc, char ** argv) {
+  if (argc != 2) {
+    fprintf(stderr, "Wrong number of argc\n");
+    return EXIT_FAILURE;
+  }
+  FILE * f = openfile(argv[1]);
+  //FILE * f = openfile("story.txt");
   step1(f);
   closefile(f);
   return EXIT_SUCCESS;
