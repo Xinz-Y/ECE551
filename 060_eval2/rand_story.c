81cc53e4d1f322411ddfc42db4f4902f5355d767
xy142 <xy142@duke.edu>
2021-10-20 01:53:11 -0400

    060

--- 060_eval2/rand_story.c
+++ 060_eval2/rand_story.c
@@ -19,7 +19,6 @@ void closefile(FILE * f) {
 }
 //make some changes for step3, s.t. adding an argument that save the word shown
 // make new change for step4, s.t. if isReuse is 0, then update the cats to a new cats
-
 void changeWord(char ** line,
                 size_t n,
                 char * start,
@@ -89,60 +88,6 @@ void changeWord(char ** line,
   free(word);
 }
 
-/* void changeWord2(char ** line, */
-/*                 size_t n, */
-/*                 char * start, */
-/*                 char * end, */
-/*                 catarray_t * cats, */
-/*                 wordsArr_t * wordsArr) { */
-/*   // get the catagoty name */
-/*   size_t len = end - start - 1; */
-/*   char * cataName = strndup(start + 1, len); */
-/*   // if wordsArr is NULL , do not need to read from wordsArr and add shown cataName to wordsArr */
-/*   const char * word; */
-/*   if (wordsArr == NULL) { */
-/*     word = chooseWord(cataName, cats); */
-/*   } */
-/*   else { */
-/*     //match the catName with word */
-/*     if (atoi(cataName) == 0) { */
-/*       // This mean cataName is not a number, we choose word from cataArr */
-/*       word = chooseWord(cataName, cats); */
-/*     } */
-/*     else { */
-/*       // read from the wordsArr */
-/*       int num = atoi(cataName); */
-/*       if ((num < 1) | (num > (int)wordsArr->n_words)) { */
-/*         fprintf(stderr, "Wrong index!\n"); */
-/*         exit(EXIT_FAILURE); */
-/*       } */
-/*       word = wordsArr->words[wordsArr->n_words - num]; */
-/*     } */
-/*     // save the word to wordsArr */
-/*     addWord2Arr(word, wordsArr); */
-/*   } */
-/*   size_t newNum; */
-/*   // compute the length of the new line */
-/*   if (strlen(cataName) < strlen(word)) { */
-/*     newNum = n + strlen(word) - strlen(cataName); */
-/*   } */
-/*   else { */
-/*     newNum = n; */
-/*   } */
-/*   // realloc a new line */
-/*   char * newLine = NULL; */
-/*   newLine = malloc(newNum * sizeof(*newLine)); */
-/*   *start = '\0'; */
-/*   char * string1 = *line; */
-/*   strcpy(newLine, string1); */
-/*   strcat(newLine, word); */
-/*   size_t backlen = strlen(end + 1); */
-/*   strncat(newLine, end + 1, backlen + 1); */
-/*   free(*line); */
-/*   *line = newLine; */
-/*   free(cataName); */
-/* } */
-
 // change arguments to  get newlines in stpe3;for step1 pcataArr and wordsArr both are NULL
 // change argumen to no reuse in step3; for step3 isReuse is 0;
 lines_t * getNewLines(FILE * f,
@@ -179,12 +124,14 @@ lines_t * getNewLines(FILE * f,
       fprintf(stderr, "The second '_' is missing\n");
       exit(EXIT_FAILURE);
     }
+    // save the filled lines into lines
     lines = realloc(lines, (n + 1) * sizeof(*lines));
     lines[n] = line;
     line = NULL;
     n++;
   }
   free(line);
+  // ues this lines_t object to save for later return
   lines_t * newLines = malloc(sizeof(*newLines));
   newLines->lines = lines;
   newLines->n_row = n;
@@ -218,7 +165,7 @@ void freeCataArr(catarray_t * pcataArr) {
   free(pcataArr->arr);
   free(pcataArr);
 }
-
+// check whethre a cataName is inside cataArr
 int checkExist(char * cataName, catarray_t * cataArr) {
   int isExist = -1;
   for (size_t i = 0; i < cataArr->n; i++) {
@@ -287,7 +234,6 @@ catarray_t * getCataArr(FILE * f) {
 }
 
 // do step3
-
 wordsArr_t * createWordsArr(void) {
   wordsArr_t * wordsArr = malloc(sizeof(*wordsArr));
   wordsArr->n_words = 0;
@@ -310,7 +256,6 @@ void freeWordsArr(wordsArr_t * wordsArr) {
   free(wordsArr);
 }
 // do step4
-
 void updateCataArr(catarray_t * cats, const char * word, char * cataName) {
   // search for the catagory
   for (size_t i = 0; i < cats->n; i++) {
--- 060_eval2/story-step4.c
+++ 060_eval2/story-step4.c
@@ -11,17 +11,12 @@ int main(int argc, char ** argv) {
   if (argc == 3) {
     // first we create catarray-t from cate/words file
     FILE * f = openfile(argv[1]);
-    //FILE * f = openfile("words.txt");
     catarray_t * pcataArr = getCataArr(f);
     //create a new catarray_t to save words showing up
     wordsArr_t * wordsArr = createWordsArr();
-    //secondly we read the template file
+    //secondly we read the template file and get new lines which has been filled
     FILE * f2 = openfile(argv[2]);
-    //FILE * f2 = openfile("story2.txt");
-
     lines_t * newLines = getNewLines(f2, pcataArr, wordsArr, 0);
-
-    //printf("we get the newLines");
     for (size_t i = 0; i < newLines->n_row; i++) {
       printf("%s\n", newLines->lines[i]);
     }
@@ -34,6 +29,7 @@ int main(int argc, char ** argv) {
   }
 
   else {
+    // check is -n is right
     if (strcmp(argv[1], "-n") != 0) {
       fprintf(stderr, "This option choiceis not -n ");
     }
