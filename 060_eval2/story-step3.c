57b051740dbbec9751ad70305f1dfd2a89a8d7a3
xy142 <xy142@duke.edu>
2021-10-19 22:00:28 -0400

    stpe3

--- 060_eval2/story-step3.c
+++ 060_eval2/story-step3.c
@@ -4,19 +4,19 @@
 #include "rand_story.h"
 
 int main(int argc, char ** argv) {
-  /* if (argc != 3) {
+  if (argc != 3) {
     fprintf(stderr, "wrong number of argv");
     return EXIT_FAILURE;
-    }*/
+  }
   // first we create catarray-t from cate/words file
-  // FILE * f = openfile(argv[1]);
-  FILE * f = openfile("words.txt");
+  FILE * f = openfile(argv[1]);
+  // FILE * f = openfile("words.txt");
   catarray_t * pcataArr = getCataArr(f);
   //create a new catarray_t to save words showing up
   wordsArr_t * wordsArr = createWordsArr();
   //secondly we read the template file
-  // FILE * f2 = openfile(argv[2]);
-  FILE * f2 = openfile("story2.txt");
+  FILE * f2 = openfile(argv[2]);
+  // FILE * f2 = openfile("story2.txt");
   lines_t * newLines = getNewLines(f2, pcataArr, wordsArr, 0);
   //printf("we get the newLines");
   for (size_t i = 0; i < newLines->n_row; i++) {
