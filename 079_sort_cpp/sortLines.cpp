55fc243dd566507d28b355274180496008ca4621
xy142 <xy142@duke.edu>
2021-10-26 14:19:10 -0400

    079

---
+++ 079_sort_cpp/sortLines.cpp
@@ -0,0 +1,46 @@
+#include <algorithm>
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <istream>
+#include <vector>
+
+std::ostream & printLines(std::vector<std::string> lines) {
+  std::vector<std::string>::iterator it = lines.begin();
+  while (it != lines.end()) {
+    std::cout << *it << std::endl;
+    ++it;
+  }
+  return std::cout;
+}
+int main(int argc, char ** argv) {
+  std::vector<std::string> lines;
+  std::string line;
+
+  if (argc == 1) {
+    // read from standard input
+    while (getline(std::cin, line).good()) {
+      lines.push_back(line);
+    }
+    if (!std::cin.eof()) {
+      std::cerr << "Can not read strings from standard input" << std::endl;
+      exit(EXIT_FAILURE);
+    }
+    std::sort(lines.begin(), lines.end());
+    printLines(lines);
+  }
+  else {
+    for (int i = 1; i < argc; i++) {
+      std::ifstream myfile;
+      myfile.open(argv[i]);
+      if (!myfile.is_open()) {
+        perror("Error open");
+        exit(EXIT_FAILURE);
+      }
+      while (getline(myfile, line)) {
+        lines.push_back(line);
+      }
+    }
+    printLines(lines);
+  }
+}
