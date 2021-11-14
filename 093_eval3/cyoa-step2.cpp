e7c7aa534e276ef1fcb6656a28919e4f63e8c0ef
xy142 <xy142@duke.edu>
2021-11-14 17:39:36 -0500

    093

--- 093_eval3/cyoa-step2.cpp
+++ 093_eval3/cyoa-step2.cpp
@@ -1,23 +1,69 @@
 #include <cstdlib>
 #include <iostream>
 
-#include "Page.hpp"
+#include "Story.hpp"
 
+class InputError : public std::exception {
+ public:
+  const char * what() const throw() {
+    return "That is not a valid choice, please try again";
+  }
+};
+
+int ConvertInput(const Page & current, std::string line) {
+  int choice_num = 0;
+  //check the input is number and a valid choice
+  try {
+    // without this if, empty line will be regarded as 0
+    if (line.empty()) {
+      throw InputError();
+    }
+    if (!IsNum(line)) {
+      throw InputError();
+    }
+    choice_num = std::atoi(line.c_str());
+    if ((size_t)choice_num > current.getPgTogo().size()) {
+      throw InputError();
+    }
+  }
+  catch (InputError & e) {
+    std::cout << e.what() << '\n';
+    getline(std::cin, line);
+    choice_num = ConvertInput(current, line);
+  }
+
+  return choice_num;
+}
 int main(int argc, char ** argv) {
-  // initialization the page object
   if (argc != 2) {
-    std::cerr << "Wrong number of argv"
-              << "\n";
+    std::cerr << "Wrong number of argv" << '\n';
     return EXIT_FAILURE;
   }
-  Page pg(argv[1]);
-
-  //int main() {
-  // Page pg("./story1/page11.txt");
-  std::cout << pg;
-  // std::vector<int> ans = pg.getPgTogo();
-  // for (size_t i = 0; i < ans.size(); ++i) {
-  //   std::cout << "pages to go are" << ans[i] << '\n';
-  // }
+  Story sy;
+  sy.makeStory(argv[1]);
+  // now we begin to read the story
+  std::vector<Page> & pages = sy.getPages();
+  Page & current = pages[0];
+  while (!current.IsWinPg() && !current.IsLosePg()) {
+    //    std::cout << "Current page is " << current.getPgNum() << '\n';
+    //std::cout << current.getPgNum() << '\n';
+    std::cout << current;
+    // read from the standard input
+    std::string line;
+    getline(std::cin, line);
+    if (!std::cin.eof() && !std::cin) {
+      std::cerr << "Can not read strings from the standard input" << '\n';
+      exit(EXIT_FAILURE);
+    }
+    int choice_num = 0;
+    //check the input is number and a valid choice
+    choice_num = ConvertInput(current, line);
+    int act_pg_num = current.getPgTogo()[choice_num - 1];
+    current = pages[act_pg_num - 1];
+  }
+  // this page is win or lose
+  //  std::cout << "Current page is " << current.getPgNum() << '\n';
+  //  std::cout << current.getPgNum() << '\n';
+  std::cout << current;
   return EXIT_SUCCESS;
 }
