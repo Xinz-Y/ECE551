9c593fc2197d55d2a1f0a83b3d3a169cf3fbff21
xy142 <xy142@duke.edu>
2021-11-15 20:44:40 -0500

    097

---
+++ 097_binsrch/search.cpp
@@ -0,0 +1,32 @@
+#include "function.h"
+
+int binarySearchForZero(Function<int, int> * f, int low, int high) {
+  //check if low and high are different sigh
+  if (low == high) {
+    return low;
+  }
+  int left = low;
+  int right = high;
+  if (f->invoke(left) < 0 && f->invoke(right) < 0) {
+    return high - 1;
+  }
+  if (f->invoke(left) > 0 && f->invoke(right) > 0) {
+    return low;
+  }
+  // since we assumen the function is monotonic, f(left)<f(right)
+  while (left < right) {
+    int mid = left + (right - left) / 2;
+    int val = f->invoke(mid);
+    // we assume the
+    if (val > 0) {
+      right = mid;
+    }
+    else if (val < 0) {
+      left = mid;
+    }
+    else {
+      return mid;
+    }
+  }
+  return left;
+}
