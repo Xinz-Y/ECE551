08e81b62f9e4709de6ca1b947c49f214fab59b4c
xy142 <xy142@duke.edu>
2021-11-20 16:42:06 -0500

    104

--- 104_huff_map/node.cpp
+++ 104_huff_map/node.cpp
@@ -1,5 +1,15 @@
 #include "node.h"
-void Node::buildMap(BitString b, std::map<unsigned,BitString> & theMap) {
-  //WRITE ME!
-}
 
+void Node::buildMap(BitString b, std::map<unsigned, BitString> & theMap) {
+  //posteroder
+  if (left != NULL) {
+    left->buildMap(b.plusZero(), theMap);
+  }
+  if (right != NULL) {
+    right->buildMap(b.plusOne(), theMap);
+  }
+  //base case
+  if (sym != NO_SYM) {
+    theMap[sym] = b;
+  }
+}
